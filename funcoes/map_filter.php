ef7f2ca02d9c87220ba128ed2386a368c0db1dd2
joao.calixto <joao.calixto@basis.com.br>
2020-10-16 15:57:19 -0300

    map filter

---
+++ funcoes/map_filter.php
@@ -0,0 +1,36 @@
+<div class="titulo">
+    Map & Filter
+</div>
+
+<?php
+    $notas = [5.8, 9.3, 4.5, 6.2, 5.9, 10, 3.7, 7.9];
+    $notasFinais1 = [];
+    
+    foreach($notas as $nota) {
+        $notasFinais1[] = round($nota);
+    }
+
+    print_r($notasFinais1);
+
+    echo "<br>";
+
+    $notasFinais2 = array_map(round, $notas);
+    print_r($notasFinais2);
+
+    $apenasOsAProvados1 = [];
+
+    foreach($notas as $nota) {
+        if($nota >= 6) {
+            $apenasOsAProvados1[] = $nota;
+        }
+    }
+
+    echo '<br>';
+    print_r($apenasOsAProvados1);
+
+    function aprovados($nota) {
+        return $nota >= 6;
+    }
+
+    $apenasOsAProvados2 = array_filter($notas, aprovados);
+    print_r($apenasOsAProvados2);
