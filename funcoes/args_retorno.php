96c8ed9c767d3e66acdd63948e249aa52e8ffd09
joao.calixto <joao.calixto@basis.com.br>
2020-10-16 11:18:21 -0300

    argumentos e retorno

--- funcoes/args_retorno.php
+++ funcoes/args_retorno.php
@@ -9,3 +9,37 @@
 
     $mensagem = obterMensagem();
     echo $mensagem;
+    echo "<br>", obterMensagem();
+    echo "<br>";
+    var_dump(obterMensagem());
+
+    function obterMensagemComNome($nome) {
+        return "Bem vindo, {$nome}!";
+    }
+
+    echo "<br>", obterMensagemComNome("João Pedro");
+
+    function soma($var1, $var2){
+        return $var1 + $var2;
+    }
+
+    $var1 = 13;
+    $var2 = 77;
+    echo "<br>", soma(4, 8);
+    echo "<br>", soma(-10, 53);
+    echo "<br>", soma($var1, $var2);
+
+    function trocaValor($var, $novoValor) {
+        $var = $novoValor;
+    }
+
+    $variavel = 1;
+    trocaValor($variavel, 7);
+    echo "<br>", $variavel; 
+
+    function trocaValorDeVerdade(&$var, $novoValor) {
+        $var = $novoValor;
+    }
+
+    trocaValorDeVerdade($variavel, 7);
+    echo "<br>", $variavel;
