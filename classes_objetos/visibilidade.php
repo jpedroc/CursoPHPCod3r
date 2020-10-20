3d95c853c910ec7bdb0938941579511ced2919fe
joao.calixto <joao.calixto@basis.com.br>
2020-10-20 12:45:54 -0300

    visibilidade

--- classes_objetos/visibilidade.php
+++ classes_objetos/visibilidade.php
@@ -3,4 +3,20 @@
 </div>
 
 <?php
-    
+    class A {
+        public $publico = "Público";
+        protected $protegido = "Protegido";
+        private $privado = "Privado";
+
+        public function mostrarA() {
+            return "Publico: {$this->publico} - Protegido: {$this->protegido} - Privado: {$this->privado}";
+        }
+
+        private function naoMostrar() {
+            return "Não vou imprimir";
+        }
+    }
+
+    $a = new A();
+    echo $a->mostrarA() . '<br>';
+    echo $a->naoMostrar() . '<br>';
