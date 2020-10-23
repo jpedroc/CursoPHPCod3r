46e56170defec0b3a7a084effe316474f76c56b3
joao.calixto <joao.calixto@basis.com.br>
2020-10-23 11:56:42 -0300

    interface

--- classes_objetos/interface.php
+++ classes_objetos/interface.php
@@ -8,21 +8,39 @@
         function respirar();
     }
 
+    interface Mamifero {
+        function mamar();
+    }
+
     interface Felino {
         function miar(): string;
     }
 
-    class Gato implements AnimalTemplate, Felino {
+    class Gato implements AnimalTemplate, Felino, Mamifero {
 
         function respirar(){
             echo 'Pelo fucinho!<br>';
         }
 
         function miar(): string {
-            return 'Miaaau!<br>';
+            return 'Miaaau!';
+        }
+
+        function mamar() {
+            return "Irei usar leite!";
         }
     }
 
     $gato = new Gato();
     $gato->respirar();
     echo $gato->miar() . '<br>';
+    echo $gato->mamar() . '<br>';
+
+    echo '<br>';
+    var_dump($gato);
+
+    echo '<br>';
+    var_dump($gato instanceof Gato);
+    var_dump($gato instanceof Felino);
+    var_dump($gato instanceof AnimalTemplate);
+    var_dump($gato instanceof Mamifero);
