a819b0cbd4e50c787c358617656c5b28c52a408a
joao.calixto <joao.calixto@basis.com.br>
2020-11-04 10:04:27 -0300

    traits 01

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
                         <li><a href="exercicio.php?dir=classes_objetos&file=abstract">Classe Abstrata</a></li>
                         <li><a href="exercicio.php?dir=classes_objetos&file=desafio_erros">Desafio Erros</a></li>
                         <li><a href="exercicio.php?dir=classes_objetos&file=final">Modificador Final</a></li>
+                        <li><a href="exercicio.php?dir=classes_objetos&file=traits_01">Traits</a></li>
                     </ul>
                 </div>
                 <div class="modulo vermelho-escuro">
